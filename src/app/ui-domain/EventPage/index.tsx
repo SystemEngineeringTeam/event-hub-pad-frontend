6740664d1e213bfac5ec36a00e0aacad8e3743a4
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-30 21:03:23 +0900

    add : イベントページの作成

---
+++ src/app/ui-domain/EventPage/index.tsx
@@ -0,0 +1,35 @@
+import { VStack } from '@chakra-ui/react';
+
+import { OtherContent } from './OtherContent';
+import { PreparationAndTodo } from './PreparationAndTodo';
+import styles from './style.module.scss';
+import { Divider } from '../Divider';
+
+export function EventPage() {
+  return (
+    <div>
+      <VStack spacing="12px" align="stretch">
+        <div className={styles.container}>
+          <div className={styles.circleContainer}>
+            <div className={styles.circle} />
+          </div>
+          <div className={styles.todoContainer}>
+            <p className={styles.title}>ダイナミックなしりとり</p>
+          </div>
+        </div>
+        <div>
+          <Divider />
+        </div>
+        <div className={styles.preparationAndTodo}>
+          <PreparationAndTodo />
+        </div>
+        <div>
+          <Divider />
+        </div>
+        <div className={styles.otherCOntainer}>
+          <OtherContent />
+        </div>
+      </VStack>
+    </div>
+  );
+}
