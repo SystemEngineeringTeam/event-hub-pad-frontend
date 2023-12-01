6ca9c2eec8cdf860b3f0dc470cc3e0528aea5264
Yusei Mizutani <yusei4520030401@gmail.com>
2023-12-02 08:48:43 +0900

    add : 定数の定義

--- src/const/todos.ts
+++ src/const/todos.ts
@@ -1,4 +1,4 @@
-import { Todo } from '@/types/todo';
+import { Todo, TodoList } from '@/types/todo';
 
 export const Todos: Todo[] = [
   {
@@ -16,5 +16,98 @@ export const Todos: Todo[] = [
     id: '3',
     event: '第1919回ゲーム大会',
     title: '先生に土下座する',
+  },
+];
+
+export const TODO_LIST: TodoList[] = [
+  {
+    id: '1',
+    title: '2023年度BBQ',
+    todos: [
+      {
+        id: '1',
+        title: '会場の予約',
+      },
+      {
+        id: '2',
+        title: '食材の手配',
+      },
+      {
+        id: '3',
+        title: '参加者の確認',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: '大富豪時ジジ抜きダウト',
+    todos: [
+      {
+        id: '1',
+        title: 'トランプの準備',
+      },
+      {
+        id: '2',
+        title: '人集め',
+      },
+      {
+        id: '3',
+        title: '参加者の確認',
+      },
+    ],
+  },
+  {
+    id: '1',
+    title: '2023年度BBQ',
+    todos: [
+      {
+        id: '1',
+        title: '会場の予約',
+      },
+      {
+        id: '2',
+        title: '食材の手配',
+      },
+      {
+        id: '3',
+        title: '参加者の確認',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: '大富豪時ジジ抜きダウト',
+    todos: [
+      {
+        id: '1',
+        title: 'トランプの準備',
+      },
+      {
+        id: '2',
+        title: '人集め',
+      },
+      {
+        id: '3',
+        title: '参加者の確認',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: '大富豪時ジジ抜きダウト',
+    todos: [
+      {
+        id: '1',
+        title: 'トランプの準備',
+      },
+      {
+        id: '2',
+        title: '人集め',
+      },
+      {
+        id: '3',
+        title: '参加者の確認',
+      },
+    ],
   },
 ];
