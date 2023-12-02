efffc7731d7e11dfbd75a690fd8ca8e4bfd451e3
Yusei Mizutani <yusei4520030401@gmail.com>
2023-12-02 18:45:58 +0900

    add : 遷移先のパスを設定

--- src/types/sideMenu.ts
+++ src/types/sideMenu.ts
@@ -2,5 +2,12 @@ import { ReactNode } from 'react';
 
 export type SideMenuItem = {
   name: string;
+  path: string;
+  icon: ReactNode;
+};
+
+export type EventMenu = {
+  name: string;
+  unit: string;
   icon: ReactNode;
 };
