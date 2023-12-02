273b50dfa90470e233a1d23762d1a4ba4dee73fa
Yusei Mizutani <yusei4520030401@gmail.com>
2023-12-02 18:37:21 +0900

    add : Todo作成ページ

---
+++ src/app/todos/[userId]/page.tsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+import { TodoList } from '../../ui-domain/TodoList';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.todoContainer}>
+        <TodoList />
+      </div>
+    </div>
+  );
+}
